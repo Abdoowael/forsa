f3ba5cfd5ff6a3f60259badde4b587f05644902f
Abdoowael <abzowael@gmail.com>
2026-06-21 21:08:57 +0300

    وو

--- penalty-react/src/components/Footer.jsx
+++ penalty-react/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             }}>
                 <div className="social-icons" style={{ flex: '1 1 auto', justifyContent: 'center' }}>
                     <a href="https://www.facebook.com/DVlottery0" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i className="fab fa-facebook-f"></i></a>
-                    <a href="#" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
+                    <a href="https://www.instagram.com/forsa.platform?igsh=aDNsMWd6cXhrY2hy" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
                     <a href="https://whatsapp.com/channel/0029Vb0mEhS9WtC1Gpl6ur1d" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp Channel"><i className="fab fa-whatsapp"></i></a>
                 </div>
                 <div className="footer-right" style={{ flex: '1 1 auto', textAlign: 'center', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '15px' }}>
